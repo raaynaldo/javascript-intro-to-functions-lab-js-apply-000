8625e212827a6e098307c03059bbfcc1259d9dbd
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-16 05:29:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string){
+  if(string.toUpperCase() === string){
+    return "I can't hear you!";
+  }
+  else if(string.toLowerCase() === string){
+    return "YES INDEED!";
+  }
+  
+}
